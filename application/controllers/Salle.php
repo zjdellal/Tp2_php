55580be9bff68f5c6f5d6524b664a9c2747aa4e9
zdjellal <chicharitoZ2>
2018-04-28 15:39:08 -0400

    ajoute de css dans chat box

--- application/controllers/Salle.php
+++ application/controllers/Salle.php
@@ -9,32 +9,38 @@
 class Salle extends CI_Controller
 {
 	private $statut;
-	public function __construct(){
+
+	public function __construct()
+	{
 		parent::__construct();
-		$this->load->library(array('ion_auth','form_validation'));
-		$this->load->helper(array('url','language'));
+		$this->load->library(array('ion_auth', 'form_validation'));
+		$this->load->helper(array('url', 'language'));
 		$this->statut = $this->ion_auth->logged_in();
 
-}
-    public function changeSalle(){
-        echo "Salle/changeSalle";
-    }
+	}
 
-    public function accueil(){
-        echo "Salle/accueil";
-    }
+	public function changeSalle($salle)
+	{
+		echo "Salle : ".$salle;
+	}
 
-    public function index()
+	public function accueil()
+	{
+		echo "Salle/accueil";
+	}
+
+	public function index()
 	{
 		$this->load->view('templates/header');
-		if ($this->statut)
-		{
-		$data['contenu'] = 'remplis moi je suis vide :/ ';
-		$this->load->view('Gabarit', $data);
+		if (!$this->statut) {
+			$data['contenu'] = 'remplis moi je suis vide :/ ';
+			//$this->load->view('Gabarit', $data);
+			$this->load->view('Salles/index',$data);
 
-		}else{
+		} else {
 			redirect('auth');
 		}
 		$this->load->view('templates/footer');
-}
+	}
+
 }
--- application/views/Salles/index.php
+++ application/views/Salles/index.php
@@ -2,8 +2,10 @@
 	<div id="acceuil_salle">
 		<?= 'ça cest notre gabarit on pourra le modifier comme on veuz j
 			 llaisser dans header dic _site_contenant et refermer dans footer' ?>
-		<?= $contenu ?>
+		<?php include_once "chat_box.php"?>
+
 	</div>
 
 
 </div><!--close site_content-->
+
